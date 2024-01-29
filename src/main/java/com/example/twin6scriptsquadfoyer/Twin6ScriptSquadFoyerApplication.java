0ca41f0041fc6e4f847374256b700ec24fcb5c87
mahdikarray <karraymahdi0@gmail.com>
2024-01-29 12:10:41 +0100

    firstcommit

---
+++ src/main/java/com/example/twin6scriptsquadfoyer/Twin6ScriptSquadFoyerApplication.java
@@ -0,0 +1,13 @@
+package com.example.twin6scriptsquadfoyer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Twin6ScriptSquadFoyerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Twin6ScriptSquadFoyerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/twin6scriptsquadfoyer/Twin6ScriptSquadFoyerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twin6scriptsquadfoyer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Twin6ScriptSquadFoyerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
