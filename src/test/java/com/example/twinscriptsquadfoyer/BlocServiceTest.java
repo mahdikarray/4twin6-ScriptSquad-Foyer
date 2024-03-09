d7d3d82d199760ba351553e28f913a83c901f5cf
WaelFerjani <wael.ferjani@esprit.tn>
2024-03-09 15:31:31 +0100

    commit_test_unitaire

--- src/test/java/com/example/twinscriptsquadfoyer/BlocServiceTest.java
+++ src/test/java/com/example/twinscriptsquadfoyer/BlocServiceTest.java
@@ -3,35 +3,23 @@ package com.example.twinscriptsquadfoyer;
 import com.example.twinscriptsquadfoyer.dao.entity.Bloc;
 import com.example.twinscriptsquadfoyer.dao.service.BlocService;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
-
-
-public class blocservicetest {
+public class BlocServiceTest {
 
     @Autowired
     private BlocService blocService;
 
     @Test
-    public void testAjouterBloc() {
+    void testAjouterBloc() {
         Bloc sampleBloc = Bloc.builder()
                 .nomBloc("Sample Bloc")
                 .capaciteBloc(10)
                 .build();
-
         Bloc savedBloc = blocService.addBloc(sampleBloc);
-
         Assertions.assertNotNull(savedBloc.getIdBloc());
-
-
     }
 }
