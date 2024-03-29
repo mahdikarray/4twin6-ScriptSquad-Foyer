d7fa42971520a2f76364b0228a417bcf5dc36785
mohamedderouiche <126126138+mohamedderouiche@users.noreply.github.com>
2024-03-29 02:39:05 +0100

    Update FoyerRestController.java

--- src/main/java/com/example/twinscriptsquadfoyer/dao/restcontroller/FoyerRestController.java
+++ src/main/java/com/example/twinscriptsquadfoyer/dao/restcontroller/FoyerRestController.java
@@ -16,7 +16,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("foyer")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 
 public class FoyerRestController {
 
