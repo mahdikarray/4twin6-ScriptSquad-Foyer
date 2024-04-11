97514b30068403aea306d7833f23436ec7116fb2
mohamedderouiche <mohamedamine.derouiche@esprit.tn>
2024-04-11 16:40:21 +0100

    update

--- src/main/java/com/example/twinscriptsquadfoyer/dao/entity/Foyer.java
+++ src/main/java/com/example/twinscriptsquadfoyer/dao/entity/Foyer.java
@@ -1,13 +1,10 @@
 package com.example.twinscriptsquadfoyer.dao.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.io.Serializable;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
